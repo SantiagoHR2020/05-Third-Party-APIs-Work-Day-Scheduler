4656cb90a2d1957c9009cbf9d1c7fea566f37261
Santiago Hincapie <santiagohr81@hotmail.com>
2020-10-17 16:51:46 -0400

    comments and finish HTML and Javascript

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # 05-Third-Party-APIs-Work-Day-Scheduler
+
+# Created by Santiago Hincapie 
+
+# Updated on 10/17/2020
+
+# Link to web site
+
+https://santiagohr2020.github.io/05-Third-Party-APIs-Work-Day-Scheduler/
+
+# Layout
+
+The day scheduler covers a working day from 9AM to 5PM
+
+# Techonogies used
+
+HTML5, CSS, Bootstrap, JavaScript, JQuary, and moment.js
+
+# Site Images
+
+# Screenshot of the start page
+
+![screenshot of the start page](assets/images/screenshot1.PNG)
--- assets/script.js
+++ assets/script.js
@@ -1,50 +1,66 @@
-$(document).ready(function(){
-    $("#currentDay").text(moment().format("dddd, MMM, Do, hA"))
+$(document).ready(function () {
+  // time
+  $("#currentDay").text(moment().format("dddd, MMM, Do, hA"));
 
-$(".saveBtn").on("click", function(){
-    var act = $(this).siblings(".description").val()
+  // listening event to save
+
+  $(".saveBtn").on("click", function () {
+    var act = $(this).siblings(".description").val();
     console.log(act);
-    
+
     var hour = $(this).parent().attr("id");
     console.log(hour);
 
     localStorage.setItem(hour, act);
 
-    console.log(localStorage)
-})
+    console.log(localStorage);
+  });
 
-$(".clear").on("click", function(){
+  // listening event to clear
+
+  $(".clear").on("click", function () {
     localStorage.clear();
     location.reload();
     console.log(localStorage);
-})
+  });
 
-function loopHour() {
-    var momentHour = moment().hours()
+// looping through the attributes
 
-    console.log(momentHour)
+  function loopHour() {
+    var momentHour = moment().hours();
 
-    $(".time-block").each(function(){
-        var ourHour = parseInt($(this).attr("id").split("-")[1])
-        console.log(ourHour)
+    console.log(momentHour);
 
-        if(ourHour<momentHour) {
-            $(this).children(".description").addClass("past");
-        }else if(ourHour === momentHour){
-            $(this).children(".description").removeClass("past");
-            $(this).children(".description").addClass("present");
-        }else{
-            $(this).children(".description").addClass("future")
-            $(this).children(".description").removeClass("past");
-            $(this).children(".description").removeClass("present");
-        }
+    $(".time-block").each(function () {
+      var ourHour = parseInt($(this).attr("id").split("-")[1]);
+      console.log(ourHour);
 
-    })
-}
+      if (ourHour < momentHour) {
+        $(this).children(".description").addClass("past");
+      } else if (ourHour === momentHour) {
+        $(this).children(".description").removeClass("past");
+        $(this).children(".description").addClass("present");
+      } else {
+        $(this).children(".description").addClass("future");
+        $(this).children(".description").removeClass("past");
+        $(this).children(".description").removeClass("present");
+      }
+    });
+  }
 
-$("#hour-9 .description").val(localStorage.getItem("hour-9"))
-$("#hour-10 .description").val(localStorage.getItem("hour-9"))
-$("#hour-11 .description").val(localStorage.getItem("hour-9"))
-$("#hour-12 .description").val(localStorage.getItem("hour-9"))
-loopHour()
-})
+// geting the info from localstorage and display
+
+  $("#hour-9 .description").val(localStorage.getItem("hour-9"));
+  $("#hour-10 .description").val(localStorage.getItem("hour-10"));
+  $("#hour-11 .description").val(localStorage.getItem("hour-11"));
+  $("#hour-12 .description").val(localStorage.getItem("hour-12"));
+  $("#hour-13 .description").val(localStorage.getItem("hour-13"));
+  $("#hour-14 .description").val(localStorage.getItem("hour-14"));
+  $("#hour-15 .description").val(localStorage.getItem("hour-15"));
+  $("#hour-16 .description").val(localStorage.getItem("hour-16"));
+  $("#hour-17 .description").val(localStorage.getItem("hour-17"));
+
+  // calling the function
+
+  loopHour();
+});
